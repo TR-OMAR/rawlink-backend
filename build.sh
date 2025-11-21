ac806044c563d6f0dccc3ef33b93a293c7e6ecdf
Abdulrahman <aljaadi.am@gmail.com>
2025-11-21 21:49:01 +0300

    Fixed.

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # Exit on error
 set -o errexit
 
+# Upgrade pip and setuptools to avoid dependency issues
+pip install --upgrade pip setuptools wheel
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
