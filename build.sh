9250be51600b9efd41d8939215d00a061b17846c
Abdulrahman <aljaadi.am@gmail.com>
2025-11-22 16:07:27 +0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,19 @@
 # Exit on error
 set -o errexit
 
-# Upgrade pip and setuptools to avoid build errors
+# 1. Upgrade pip
 pip install --upgrade pip setuptools wheel
 
-# Install dependencies
+# 2. Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
+# 3. Force creation of migrations for 'api' app if missing
+# This fixes the "Dependency on app with no migrations" error
+python manage.py makemigrations api
+python manage.py makemigrations
+
+# 4. Collect static files
 python manage.py collectstatic --no-input
 
-# Run database migrations
+# 5. Run migrations
 python manage.py migrate
