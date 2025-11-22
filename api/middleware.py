ccdd2d9e4b64b00f28eb67e0345f0536c3b8da82
Abdulrahman <aljaadi.am@gmail.com>
2025-11-22 15:56:40 +0300

    Fix AppRegistryNotReady by using lazy imports in middleware

--- api/middleware.py
+++ api/middleware.py
@@ -1,16 +1,17 @@
-from django.contrib.auth.models import AnonymousUser
 from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
 from rest_framework_simplejwt.tokens import AccessToken
-# REMOVE THIS LINE: from api.models import User
 from urllib.parse import parse_qs
+
+# Move these imports inside functions to avoid AppRegistryNotReady
+# from django.contrib.auth.models import AnonymousUser 
+# from api.models import User
 
 @database_sync_to_async
 def get_user(token_key):
+    from django.contrib.auth.models import AnonymousUser
+    from api.models import User
     try:
-        # ADD THIS LINE HERE INSTEAD:
-        from api.models import User
-        
         token = AccessToken(token_key)
         user_id = token['user_id']
         return User.objects.get(id=user_id)
@@ -19,7 +20,10 @@ def get_user(token_key):
 
 class JwtAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
+        from django.contrib.auth.models import AnonymousUser
+        
         try:
+            # Get the token from the query string
             query_string = scope.get("query_string", b"").decode("utf-8")
             params = parse_qs(query_string)
             token = params.get("token", [None])[0]
