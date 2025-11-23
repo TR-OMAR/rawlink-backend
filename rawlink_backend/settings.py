8af3a6cf11a7b295f30bef3d666e91ed034dedb4
Abdulrahman <aljaadi.am@gmail.com>
2025-11-23 16:33:36 +0300

    Fix syntax error in installed apps

--- rawlink_backend/settings.py
+++ rawlink_backend/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary_storage',
-    'cloudinary'
+    'cloudinary',
 
     # Local app
     'api.apps.ApiConfig',
